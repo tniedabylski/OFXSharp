72fb2ebbec1f78eb2409b2c7c8de10bd2b1f4d77
Tomasz Niedabylski <tomasz.niedabylski@gmail.com>
2019-01-26 21:31:28 -0800

    Plug tests into nunit

--- source/OFXSharp.Tests/CanParser.cs
+++ source/OFXSharp.Tests/CanParser.cs
@@ -4,17 +4,20 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace OFXSharp.Tests
 {
     public class CanParser
     {
+        [Test]
         public void CanParserItau()
         {
             var parser = new OFXDocumentParser();
             var ofxDocument = parser.Import(new FileStream(@"itau.ofx", FileMode.Open));
         }
 
+        [Test]
         public void CanParserSantander()
         {
             var parser = new OFXDocumentParser();
